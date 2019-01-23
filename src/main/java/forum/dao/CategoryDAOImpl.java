ccc6768a40cbccd3b3026362b528100a5839b3df
Mikko Rajala <mikkorajala@hotmail.com>
2019-01-23 13:07:26 +0200

    Modify CategoryDAO to use Criteria API for retrieving all categories

--- src/main/java/forum/dao/CategoryDAOImpl.java
+++ src/main/java/forum/dao/CategoryDAOImpl.java
@@ -2,9 +2,13 @@ package forum.dao;
 
 import java.util.List;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -22,12 +26,24 @@ public class CategoryDAOImpl implements CategoryDAO {
 		
 		Session currentSession = sessionFactory.getCurrentSession();
 		
+		CriteriaBuilder cb = currentSession.getCriteriaBuilder();
+		
+		CriteriaQuery<Category> cq = cb.createQuery(Category.class);
+		
+		Root<Category> rootEntry = cq.from(Category.class);
+		
+		CriteriaQuery<Category> all = cq.select(rootEntry);
+		
+		TypedQuery<Category> allQuery = currentSession.createQuery(all);
+		
+		/*
 		Query<Category> theQuery =
 				currentSession.createQuery("from Category", Category.class);
 		
 		List<Category> categories = theQuery.getResultList();
+		*/
 
-		return categories;
+		return allQuery.getResultList();
 	}
 
 
