0135fb49e18e0bcb0b2d0e0c1b1e9451a46a3670
Mikko Rajala <mikkorajala@hotmail.com>
2019-01-23 18:19:23 +0200

    Add Reply entity

---
+++ src/main/java/forum/entity/Reply.java
@@ -0,0 +1,70 @@
+package forum.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reply")
+public class Reply {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "comment")
+	private String comment;
+
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+							CascadeType.DETACH, CascadeType.REFRESH })
+	@JoinColumn(name = "thread_id")
+	private Thread thread;
+	
+	public Reply() {
+		
+	}
+
+	public Reply(int id, String comment) {
+		this.id = id;
+		this.comment = comment;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Thread getThread() {
+		return thread;
+	}
+
+	public void setThread(Thread thread) {
+		this.thread = thread;
+	}
+
+	@Override
+	public String toString() {
+		return "Reply [id=" + id + ", comment=" + comment + ", thread=" + thread + "]";
+	}
+	
+	
+
+}
